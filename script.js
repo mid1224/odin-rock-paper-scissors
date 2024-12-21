3f35d53b57c8d877b9fc3b46fc0eac5cbb46eec9
mid1224 <61130938+mid1224@users.noreply.github.com>
2024-12-21 20:39:07 +0700

    Add playRound() function
    Play a round of RockPaperScissor, print on console result of the round & current scores

--- script.js
+++ script.js
@@ -17,4 +17,52 @@ function getComputerChoice ()
     }
 }
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
+
+function getHumanChoice ()
+{
+    let choice = prompt("Rock, paper, or scissor?: ");
+    let choiceLowered = choice.toLowerCase();
+    let finalChoice = choiceLowered.trim();
+
+    if (finalChoice === "rock" || finalChoice === "paper" || finalChoice === "scissor")
+    {
+        return finalChoice;
+    }
+
+    return "Invalid";
+}
+
+//console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound()
+{
+    let compChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+
+    if (humanChoice === "Invalid")
+    {
+        console.log("Invalid Input! The round was forfeited!");
+        return;
+    }
+    else if (humanChoice === compChoice)
+    {
+        console.log("Tied! Both are " + humanChoice);
+        return;
+    }
+    else if ((humanChoice === "rock" && compChoice === "paper") || (humanChoice === "paper" && compChoice === "scissor") || (humanChoice === "scissor" && compChoice === "rock"))
+    {
+        console.log("You lose! " + compChoice + " beats " + humanChoice);
+        console.log("Current score: You - " + humanScore + " Computer - " + (++computerScore))
+        return;
+    }
+    else if ((humanChoice === "rock" && compChoice === "scissor") || (humanChoice === "paper" && compChoice === "rock") || (humanChoice === "scissor" && compChoice === "paper"))
+    {
+        console.log("You win! " + humanChoice + " beats " + compChoice);
+        console.log("Current score: You - " + (++humanScore) + " Computer - " + computerScore)
+        return;
+    }
+}
